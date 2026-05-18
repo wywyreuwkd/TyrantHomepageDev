37cf2c808c808adc8ff684d2a1fa8e51aecbcdbd
KANGJUNHO <wywyreuwkd@naver.com>
2026-05-19 00:42:27 +0900

    푸터 임시 제거

--- src/app/(user page)/layout.tsx
+++ src/app/(user page)/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/layout/header"
-import { Footer } from "@/components/layout/footer"
+// import { Footer } from "@/components/layout/footer"
 
 export default function UserPageLayout({
   children,
